3ec12227559711c43e712e65a3338d071e06ef18
GaryGSC <garygsc@gmail.com>
2017-03-30 04:00:56 +0000

    add new user button is up on userlist

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 // Mongo
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('localhost:27017/users');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
